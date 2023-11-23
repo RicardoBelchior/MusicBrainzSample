e0e21bc45e53cd2e18e7627350051110c2a2d7ce
Ricardo Lagido <ricardolagido@gmail.com>
2023-11-23 17:10:05 +0100

    Update versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.rbelchior.dicetask"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.rbelchior.dicetask"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
