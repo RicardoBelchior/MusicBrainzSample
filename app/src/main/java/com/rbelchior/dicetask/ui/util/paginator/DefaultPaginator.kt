18e584811ffbf8cde75a838445c5cbe5178737b8
Ricardo Belchior <ricardolagido@gmail.com>
2025-02-26 20:44:48 +0000

    Update dependencies

--- app/src/main/java/com/rbelchior/dicetask/ui/util/paginator/DefaultPaginator.kt
+++ app/src/main/java/com/rbelchior/dicetask/ui/util/paginator/DefaultPaginator.kt
@@ -2,11 +2,11 @@ package com.rbelchior.dicetask.ui.util.paginator
 
 class DefaultPaginator<Key, Page>(
     private val initialKey: Key,
-    private inline val onLoadUpdated: (Boolean) -> Unit,
-    private inline val onRequest: suspend (nextKey: Key) -> Result<Page>,
-    private inline val getNextKey: suspend (Page) -> Key,
-    private inline val onError: suspend (Throwable) -> Unit,
-    private inline val onSuccess: suspend (page: Page, newKey: Key) -> Unit
+    private val onLoadUpdated: (Boolean) -> Unit,
+    private val onRequest: suspend (nextKey: Key) -> Result<Page>,
+    private val getNextKey: suspend (Page) -> Key,
+    private val onError: suspend (Throwable) -> Unit,
+    private val onSuccess: suspend (page: Page, newKey: Key) -> Unit
 ) : Paginator<Key, Page> {
 
     private var currentKey = initialKey
